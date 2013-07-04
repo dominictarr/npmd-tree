6f3d580f4c52edb240104450ccca284be1010e9c
Dominic Tarr <dominic.tarr@gmail.com>
2013-07-03 21:37:54 -0700

    tidy

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ var path = require('path')
 var pull = require('pull-stream')
 var pfs  = require('pull-fs')
 
+var clean = require('./clean')
+
+function filter () {
+  return pull(pull.filter(),
+    pull.map(function (pkg) {
+      return {
+        name   : pkg.name,
+        version: pkg.version,
+        path   : pkg.path
+      }
+    })
+  )
+}
+
+function first (cb) {
+  var f
+  return pull.drain(
+    function (data) { f = data; return false },
+    function (err) { cb(err === true ? null : err, f) }
+  )
+}
+
 function readJson () {
   return pull.asyncMap(function (file, cb) {
     fs.readFile(file, 'utf8', function (err, data) {
@@ -14,24 +36,6 @@ function readJson () {
       cb(null, json)
     })
   })
-}
-
-function filter () {
-  return pull.map(function (pkg) {
-    return {
-      name   : pkg.name,
-      version: pkg.version,
-      path   : pkg.path
-    }
-  })
-}
-
-function first (cb) {
-  var f
-  return pull.drain(
-    function (data) { f = data; return false },
-    function (err) { cb(err === true ? null : err, f) }
-  )
 }
 
 function findPackage (dir, cb) {
@@ -61,6 +65,7 @@ function ls (dir, cb) {
     pull.filter(Boolean),
     readJson(),
     filter(),
+    pull.unique('name'),
     pull.reduce(function (obj, val) {
       if(!obj[val.name])
         obj[val.name] = val
@@ -71,58 +76,9 @@ function ls (dir, cb) {
   )
 }
 
-var fields = 'name,version,from,gypfile,shasum'.split(',')
 
-function clean (t) {
-  var deps = t.dependencies
-  var _deps = t.tree || {}
-
-  var shasum = t.dist && t.dist.shasum
-  for(var k in t)
-    if(!~fields.indexOf(k))
-      delete t[k]
-
-//  delete t.tree
-//  delete t._parent
-//  delete t.description
-//  delete t.devDependencies
-//  delete t.tree
-//  delete t.scripts
-//  delete t.parent
-//  delete t.time
-//  delete t.size
-//  delete t.readme
-//  delete t.author
-//  delete t.homepage
-//  delete t._from
-//  delete t._resolved
-//  delete t.license
-//  delete t.bugs
-//  delete t.repository
-//  delete t.readmeFilename
-//  delete t._id
-//  delete t.keywords
-//  delete t.testling
-//  if(t.dist)
-//    t.shasum = t.dist.shasum
-//  delete t.dist
-//  delete t.path
-//  delete t.dependencies
-
-  t.shasum = shasum
-
-  for(var k in _deps) {
-    _deps[k].from = deps[k]
-    clean(_deps[k])
-  }
-
-  t.dependencies = _deps
-
-  return t
-}
-
-
-//todo: create the same datastructure as resolve.
+//creates the same datastructure as resolve,
+//selecting all dependencies...
 
 function tree (dir, cb) {
   if(!cb) cb = dir, dir = null
@@ -134,9 +90,7 @@ function tree (dir, cb) {
         pkg.tree = {}
         return pull(
           pfs.readdir(path.resolve(pkg.path, 'node_modules')),
-          pull.filter(),
           pfs.resolve('package.json'),
-          pfs.isFile(),
           readJson(),
           pull.filter(function (_pkg) {
             if(!_pkg) return
@@ -158,20 +112,30 @@ exports.tree = tree
 exports.findPackage = findPackage
 exports.ls = ls
 
-exports.db = function noop () {}
-exports.commands = function (db) {
-  db.commands.tree = function (config, cb) {
-    tree(config.installPath, function (err, tree) {
-      if(err) throw err
-      console.log(JSON.stringify(tree, null, 2))
-      cb()
-    })
-  }
-  db.commands.ls = function (config, cb) {
-    ls(config.installPath, function (err, tree) {
-      if(err) throw err
-      console.log(JSON.stringify(tree, null, 2))
-      cb()
-    })
-  }
+
+exports.cli = function (db) {
+  db.commands.push(function (db, config, cb) {
+    var args = config._.slice()
+    var cmd = args.shift()
+  
+    if(cmd == 'tree') {
+      tree(config.installPath, function (err, tree) {
+        if(err) throw err
+        console.log(JSON.stringify(tree, null, 2))
+        cb()
+      })
+    }
+    else if(cmd == 'ls')
+      ls(config.installPath, function (err, tree) {
+        if(err) throw err
+        console.log(JSON.stringify(tree, null, 2))
+        cb()
+      })
+    else
+      return
+
+    return true
+  })
+
 }
+
